389f88c551b13a9a842171a59f3d0abb46cdda4a
Krish4U2 <krishkumaramangalam@gmail.com>
2021-07-21 23:43:59 +0530

    week2 files

---
+++ week2/src/scripts/server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from pickle import TRUE
+import rospy 
+import numpy as np
+from week2.srv import values, valuesResponse
+  
+class Unicycle:
+    
+    def __init__(self, x: float, y: float, theta: float):
+        self.x = x
+        self.y = y
+        self.theta = theta
+        
+
+        # Store the points of the trajectory to plot
+        self.x_points = [self.x]
+        self.y_points = [self.y]
+    # def step(self, v: float, w: float):
+    def calc_trajectory(self,request):
+       v = request.v
+       w = request.w
+       for i in range(50):
+        self.theta += w*(0.05)
+        self.x += v*np.cos(self.theta)
+        self.y += v*np.sin(self.theta)
+        self.x_points.append(self.x)
+        self.y_points.append(self.y)
+       return valuesResponse(self.x_points, self.y_points)
+if __name__ == "__main__":
+   
+    rospy.init_node('server',anonymous = TRUE)
+    print('Ready!')
+    obj = Unicycle(0,0,0)
+    # obj.trajectory(1, 0.5)
+    s = rospy.Service('trajectory', values, obj.calc_trajectory)
+    rospy.spin()
+# def trajectory(request):
+#     x_points = []
+#     y_points = []
+
+#     v = request.v
+#     w = request.w
+
+#     for i in range(50):
+#         theta += w*(0.05)
+#         x += v*np.cos(theta)
+#         y += v*np.sin(theta)
+
+#         x_points.append(x)
+#         y_points.append(y)
+#         return x_points,y_points,theta  
